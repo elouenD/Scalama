b5d3b30adc8b95024b1c484a3d5c6debeec69d49
elouen.dumas.pilhou <elouen.dumas.pilhou@gmail.com>
2018-12-14 17:48:27 +0100

    feature: Q3 SF and Paris

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -10,10 +10,19 @@ object Hello extends Greeting with App {
   println(greeting)
 
   val pathToFile = "data/reduced-voiture.json"
+  val PARISLATITUDEMIN = 48.806555
+  val PARISLONGITUDEMIN = 2.239097
+  val SFLATITUDEMIN = 37.696036
+  val SFLONGITUDEMIN = -122.509512
+
+  val PARISLATITUDEMAX = 48.903012
+  val PARISLONGITUDEMAX = 2.419856
+  val SFLATITUDEMAX = 37.814928
+  val SFLONGITUDEMAX = -122.374243
 
   val test = loadData()
   println("test")
-  println(PourcentageFailBecauseOfFuel())
+  println(CarsIsFailingMoreInParis())
   println("test")
 
 
@@ -32,11 +41,19 @@ object Hello extends Greeting with App {
       .cache()
   }
 
+  /**
+    *
+    * @return list de voiture qui fail
+    */
   def carsFailsRDD() : RDD[Voiture] = {
     loadData()
       .filter(car => car.isFailing)
   }
 
+  /**
+    *
+    * @return temperature moyenne des voitures qui fail
+    */
   def carsFailsAverageTemperature(): Double = {
     carsFailsRDD
       // rajouter un check is carsFails retourne rien, sinon ça pête
@@ -44,11 +61,19 @@ object Hello extends Greeting with App {
       .sum / carsFailsRDD.count()
   }
 
+  /**
+    *
+    * @return array of cars moving
+    */
   def carsMovingRDD() : RDD[Voiture] = {
     loadData()
       .filter(car => car.isMoving)
   }
 
+  /**
+    *
+    * @return average temperatur for moving cars
+    */
   def carsMovingAverageTemperature(): Double = {
     val carsMovingRDD = carsFailsRDD()
     carsMovingRDD
@@ -56,19 +81,67 @@ object Hello extends Greeting with App {
       .sum / carsMovingRDD.count()
   }
 
+  /**
+    *
+    * @return is the temperatur for failing cars is higher
+    */
   def CheckTempatureFailsCarsIsHigher(): Boolean = {
     carsFailsAverageTemperature() > carsMovingAverageTemperature()
   }
 
+  // question 4
+  /**
+    *
+    * @return nombre de voiture en fail à cause du carburant
+    */
   def nbCarsFailingWithNoFuel() : Double = {
     carsFailsRDD()
       .filter(car => car.fuelInTank == 0)
       .count()
   }
 
+  /**
+    *
+    * @return le pourcentage de voiture qui fail à cause du carburant
+    */
   def PourcentageFailBecauseOfFuel(): Double = {
     nbCarsFailingWithNoFuel() / carsFailsRDD().count()
   }
+
+  // question 3
+  /**
+    *
+    * @return nombre de cars qui fail à Paris
+    */
+  def nbCarsFailingInParis(): Double = {
+    carsFailsRDD()
+      .filter(cars => (PARISLATITUDEMIN <= cars.lat && cars.lat <= PARISLATITUDEMAX )
+        && (PARISLONGITUDEMIN <= cars.long && cars.long <= PARISLONGITUDEMAX)
+      )
+      .count()
+  }
+
+  /**
+    *
+    * @return nombre de cars qui fail à San Fransisco
+    */
+  def nbCarsFailingInSanFransisco(): Double = {
+    carsFailsRDD()
+      .filter(cars => (SFLATITUDEMIN <= cars.lat && cars.lat <=  SFLATITUDEMAX )
+        && (SFLONGITUDEMIN <= cars.long && cars.long <= SFLONGITUDEMAX)
+      )
+      .count()
+  }
+
+
+  /**
+    *
+    * @return true si il y a plus de cars qui fail à Paris, sinon false
+    */
+  def CarsIsFailingMoreInParis(): Boolean = {
+    nbCarsFailingInParis() > nbCarsFailingInSanFransisco()
+  }
+
 
 
 }
